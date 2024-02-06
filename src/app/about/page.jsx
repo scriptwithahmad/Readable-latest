e997c9379b28eed7a67bf3ab9041b4c260e8c518
Muhammadh Ahmad <scriptwithahmad@gmail.com>
2024-02-06 11:54:45 +0500

    basic sturctor of app like layout, routes, api's done

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <h1>About Page Here</h1>
+    </>
+  );
+};
+
+export default page;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <>
+      <h1>Navbar Here</h1>
+    </>
+  );
+};
+
+export default Nav;
