13a3fa51ff8d056258435d547dfce18d80e17354
Muhammadh Ahmad <scriptwithahmad@gmail.com>
2024-02-09 18:43:40 +0500

    fetch server side data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+async function getData() {
+  const res = await fetch("http://localhost:3000/api/users", {
+    next: { revalidate: 10 },
+  });
 
-const Header = () => {
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+const Header = async () => {
+  const data = await getData();
+
   return (
     <>
       <h1>Header Section</h1>
