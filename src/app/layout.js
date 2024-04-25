bbb90416d89db98e9033fb0d69843bd535bd4f1d
Muhammadh Ahmad <scriptwithahmad@gmail.com>
2024-04-25 12:14:24 +0500

    Minor Changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -47,7 +47,7 @@ export default function RootLayout({ children }) {
               </>
             ) : (
               <div className={sans.className}>
-                {/* <Nav /> */}
+                <Nav />
                 <Particles />
                 {children}
                 <Footer />
