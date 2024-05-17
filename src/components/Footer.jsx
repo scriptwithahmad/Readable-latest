caaa96b25523b0498a965b02b239e8c8e15473a6
Ahmad <scriptwithahmad@gmail.com>
2024-05-17 23:42:25 +0500

    footer change dark to light only theme

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Meriend = Merienda({
 
 const Footer = () => {
   return (
-    <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+    <footer className="bg-white rounded-lg globalShadow2 m-4">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <Link
@@ -21,12 +21,12 @@ const Footer = () => {
               src="https://res.cloudinary.com/dmyrswz0r/image/upload/v1713427098/blog-image/favicon_jcqo9l.png"
             />
             <span
-              className={`${Meriend.className} self-center text-gray-600 text-2xl font-semibold whitespace-nowrap dark:text-white`}
+              className={`${Meriend.className} self-center text-gray-600 text-2xl font-semibold whitespace-nowrap `}
             >
               Readable
             </span>
           </Link>
-          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
             <li>
               <Link href="/about" className="hover:underline me-4 md:me-6">
                 About
@@ -52,9 +52,9 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
         <div className="flex items-center justify-between">
-          <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          <span className="block text-sm text-gray-500 sm:text-center">
             © 2024{" "}
             <Link href="/" className="hover:underline">
               Readable™
